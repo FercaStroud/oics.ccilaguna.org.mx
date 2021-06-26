e026a50d34bf96c659ec70ee04bab737c0217d24
Fernando Cárdenas <fernandocardenas@Fernandos-iMac.local>
2021-06-26 11:37:03 -0500

    grafíca de dona

---
+++ src/components/DoughnutChart.vue
@@ -0,0 +1,42 @@
+<script>
+import {Doughnut} from 'vue-chartjs'
+
+export default {
+  extends: Doughnut,
+  mounted() {
+    this.renderChart(
+        {
+          labels: ['Transparencia y Acceso a la Información', 'Participación Ciudadana', 'Rendición de Cuentas', 'Rendimiento'],
+          datasets: [
+            {
+              backgroundColor: [
+                '#41B883',
+                '#E46651',
+                '#00D8FF',
+                '#DD1B16'
+              ],
+              data: [25, 25, 25, 25]
+            }
+          ]
+        },
+        {
+          title: {
+            display: false,
+          },
+          tooltips: {
+            callbacks: {
+              label: function(tooltipItem, data) {
+                return data.labels[tooltipItem.index];
+              }
+            }
+          },
+          onClick: (e, data) => {
+            this.$emit('updateTextInDoughnut', data)
+          },
+          responsive: true,
+          maintainAspectRatio: false
+        }
+    )
+  }
+}
+</script>
--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -24,44 +24,21 @@
               alt="Responsive image"
             )
         b-row(align-v="center")
-          b-col(md="12")
+          b-col(md="12" style="text-align:left")
             strong.title Ejes evaluados:
-          b-col(md="12")
-            .accordion(role='tablist')
-              b-card.mb-1(no-body='')
-                b-card-header.p-1(header-tag='header' role='tab')
-                  b-button(block='' v-b-toggle.accordion-1='' type="dark" style="background-color:#09306c") TRANSPARENCIA Y ACCESO A LA INFORMACIÓN
-                b-collapse#accordion-1(visible='' accordion='my-accordion' role='tabpanel')
-                  b-card-body
-                    .text-left.text-container
-                      p(style="text-align:justify") Construir un buen gobierno implica la apertura de su actuación al escrutinio público a través de mecanismos que garanticen a los ciudadanos accesibilidad total al ejercicio de la función pública e impulsen esquemas de corresponsabilidad en la elaboración de políticas públicas encaminadas a la formación de una conducta ética en el funcionamiento administrativo, transparencia en la asignación y el manejo de recursos públicos, así como la rendición de cuentas de acuerdo a las metas establecidas en el corto,mediano y largo plazos.
-                      p(style="text-align:justify") La TRANSPARENCIA hace referencia a la disponibilidad de información a través de la cual el OIC hace visibles y accesibles -de manera suficiente, oportuna, clara y adecuada susacciones,políticas, procedimientos y decisiones.
-              b-card.mb-1(no-body='')
-                b-card-header.p-1(header-tag='header' role='tab')
-                  b-button(block='' v-b-toggle.accordion-2='' type="dark" style="background-color:#09306c") PARTICIPACIÓN CIUDADANA:
-                b-collapse#accordion-2(accordion='my-accordion' role='tabpanel')
-                  b-card-body
-                    .text-left.text-container
-                      p(style="text-align:justify") Se refiere a la dimensión de apertura del OIC para incorporarmecanismos e instancias de cooperación y de articulación que faciliten elejercicio de un controlactivo por parte de los ciudadanos y organizaciones de la sociedad civil. Es imprescindiblegarantizar la participación activa de la ciudadanía a fin de perfeccionar la calidad del control, y su involucramiento coadyuva a una mayor cultura de transparencia gubernamental.
-              b-card.mb-1(no-body='')
-                b-card-header.p-1(header-tag='header' role='tab')
-                  b-button(block='' v-b-toggle.accordion-3='' type="dark" style="background-color:#09306c") RENDICIÓN DE CUENTAS
-                b-collapse#accordion-3(accordion='my-accordion' role='tabpanel')
-                  b-card-body
-                    .text-left.text-container
-                      p(style="text-align:justify") Se refiere a los ejercicios mediante los cuales el OIC se responsabiliza de su accionar frente a la ciudadanía -principal beneficiaria de la tarea de control,a través de dispositivos institucionales que permiten dar a conocer intensamente losprocedimientosy actividades que desempeña.En particular, la rendición de cuentas comprende la existencia de prácticas y mecanismos através de los cuales la entidades activamente transparente respecto de su funcionamiento interno,iniciando procedimientos de comunicación con el público y difundiendo ampliamente sus procesos y resultados.
-              b-card.mb-1(no-body='')
-                b-card-header.p-1(header-tag='header' role='tab')
-                  b-button(block='' v-b-toggle.accordion-4='' type="dark" style="background-color:#09306c") RENDIMIENTO
-                b-collapse#accordion-4(accordion='my-accordion' role='tabpanel')
-                  b-card-body
-                    .text-left.text-container
-                      p(style="text-align:justify") Se refiere al desempeño del OIC respecto de las facultades y funciones quele competen, así como la operatividad en los procedimientos que lleva a cabo. El rendimiento comprendeel funcionamiento mismo de la institución. Contempla la existencia de prácticas y mecanismos de gestión, y examina los procedimientos que permiten garantizar una labor de control imparcial y efectiva que maximice un desempeño integral sólido.
-
+          b-col(md="6")
+            doughnut-chart(
+              v-on:updateTextInDoughnut="updateTextInDoughnut"
+            )
+          b-col(md="6")
+            p(style="text-align:left !important;")
+              strong {{ textInDoughnut.title }}
+            p(style="text-align:justify !important;") {{ textInDoughnut.description }}
         b-row(align-v="center")
           b-col(md="12")
             .text-left.text-container
-              p La independencia de la entidad resulta una precondición para alcanzar sus objetivos y misión,pues de ello se deriva el nivel de autonomía del que dispondrá para administrar su labor y desarrollar sus funciones.
+              br/
+              p(style="text-align:justify !important;") La independencia de la entidad resulta una precondición para alcanzar sus objetivos y misión,pues de ello se deriva el nivel de autonomía del que dispondrá para administrar su labor y desarrollar sus funciones.
     section#mediciones.p-5(style="background: ghostwhite")
       .container
         b-row(align-v="center")
@@ -298,12 +275,14 @@
 
 <script>
 import BarChart from "../components/BarChart";
+import DoughnutChart from "../components/DoughnutChart";
 
 export default {
   name: 'Landing',
-  components: {BarChart},
+  components: {BarChart, DoughnutChart},
   data: function () {
     return {
+      textInDoughnut:{title:'',description:''} ,
       labels: [],
       rates: [],
       barChart:false,
@@ -991,6 +970,37 @@ export default {
     }
   },
   methods: {
+    updateTextInDoughnut: function(data){
+      switch (data[0]._index){
+        case 0:
+          this.textInDoughnut = {
+            title: 'TRANSPARENCIA Y ACCESO A LA INFORMACIÓN',
+            description: 'Construir un buen gobierno implica la apertura de su actuación al escrutinio público a través de mecanismos que garanticen a los ciudadanos accesibilidad total al ejercicio de la función pública e impulsen esquemas de corresponsabilidad en la elaboración de políticas públicas encaminadas a la formación de una conducta ética en el funcionamiento administrativo, transparencia en la asignación y el manejo de recursos públicos, así como la rendición de cuentas de acuerdo a las metas establecidas en el corto,mediano y largo plazos.\n' +
+                '\n' +
+                'La TRANSPARENCIA hace referencia a la disponibilidad de información a través de la cual el OIC hace visibles y accesibles -de manera suficiente, oportuna, clara y adecuada susacciones,políticas, procedimientos y decisiones.',
+          }
+          break;
+        case 1:
+          this.textInDoughnut = {
+            title: 'PARTICIPACIÓN CIUDADANA',
+            description: 'Se refiere a la dimensión de apertura del OIC para incorporarmecanismos e instancias de cooperación y de articulación que faciliten elejercicio de un controlactivo por parte de los ciudadanos y organizaciones de la sociedad civil. Es imprescindiblegarantizar la participación activa de la ciudadanía a fin de perfeccionar la calidad del control, y su involucramiento coadyuva a una mayor cultura de transparencia gubernamental.',
+          }
+          break;
+        case 2:
+          this.textInDoughnut = {
+            title: 'RENDICIÓN DE CUENTAS',
+            description: 'Se refiere a los ejercicios mediante los cuales el OIC se responsabiliza de su accionar frente a la ciudadanía -principal beneficiaria de la tarea de control,a través de dispositivos institucionales que permiten dar a conocer intensamente losprocedimientosy actividades que desempeña.En particular, la rendición de cuentas comprende la existencia de prácticas y mecanismos através de los cuales la entidades activamente transparente respecto de su funcionamiento interno,iniciando procedimientos de comunicación con el público y difundiendo ampliamente sus procesos y resultados.',
+          }
+          break;
+        case 3:
+          this.textInDoughnut = {
+            title: 'RENDIMIENTO',
+            description: 'Se refiere al desempeño del OIC respecto de las facultades y funciones quele competen, así como la operatividad en los procedimientos que lleva a cabo. El rendimiento comprendeel funcionamiento mismo de la institución. Contempla la existencia de prácticas y mecanismos de gestión, y examina los procedimientos que permiten garantizar una labor de control imparcial y efectiva que maximice un desempeño integral sólido.',
+          }
+          break;
+
+      }
+    },
     onChangeMeasurement: function () {
       this.selectedEntry = '';
       this.selectedQuestion = '';
